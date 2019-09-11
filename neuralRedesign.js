99e5e800381e365d406d15e6b6c931cfdab75999
LieuweJS <baronlieuwe38@gmail.com>
2019-09-11 14:40:02 +0200

    added function with parameter settings

--- neuralRedesign.js
+++ neuralRedesign.js
@@ -1,1 +1,8 @@
-//here will be the redesigned neural network system
+/**
+  *@param {settings} object with the settings for the neural network function
+  */
+function neuralEvolution(settings) {
+  let mutationRate = 0.2;
+  let networkStructureType = 'fullyConnected';
+  let networkStructure = [2,[2,2],1];
+}
