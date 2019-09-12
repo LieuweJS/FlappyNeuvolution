755250012c16fc7f3f7c5d60397684a0842d49d0
LieuweJS <baronlieuwe38@gmail.com>
2019-09-12 08:09:20 +0200

    update

--- neuralRedesign.js
+++ neuralRedesign.js
@@ -1,16 +1,28 @@
 /**
   *@param {settings} object with the settings for the neural network function
   */
-function neuralEvolution(settings) {
+let neuralEvolution = function(settings) {
   //these are the default settings
   const main = this;
   main.settings {
-    population = 50
-    network = [1,[1],1];
-    mutationRate = 0.2;
+    population = 50,
+    network = [1,[1],1],
+    mutationRate = 0.2,
     activation = 'ELU'
+
   }
+  
+  main.setSettings = function(settings) {
+    for(let i in settings) {
+      if(this.settings[i] != undefined) {
+        main.settings[i] = settings[i];
+      }
+    }
+  }
+
   let breed = function(father, mother, childAmount) {
-    
+    for() {
+
+    }
   }
 }
